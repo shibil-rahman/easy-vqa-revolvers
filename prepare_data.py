8d4c1ccc9acd96aa2067f74734c243f6f9a98d7e
shibil-rahman <shibilr6@gmail.com>
2021-12-30 17:51:53 +0530

    prepare_data.py edited

--- prepare_data.py
+++ prepare_data.py
@@ -18,27 +18,24 @@ def setup(use_data_dir):
       return (texts, answers, image_ids)
     train_qs, train_answers, train_image_ids = read_questions('convertcsv.json')
     test_qs, test_answers, test_image_ids = read_questions('convertcsv.json')
-	print(f'Read {len(train_qs)} training questions and {len(test_qs)} testing questions.')
+  print(f'Read {len(train_qs)} training questions and {len(test_qs)} testing questions.')
 	
-	print('\n--- Reading answers...')
-	if use_data_dir:
+  print('\n--- Reading answers...')
+  if use_data_dir:
     # Read answers from data/ folder
-		with open('answers.txt', 'r') as file:
-		all_answers = [a.strip() for a in file]
-	else:
-    # Read answers from the easy-vqa package
-		all_answers = get_answers()
-	num_answers = len(all_answers)
-	print(f'Found {num_answers} total answers:')
-	print(all_answers)
+	  with open('answers.txt', 'r') as file:
+	    all_answers = [a.strip() for a in file]
+  num_answers = len(all_answers)
+  print(f'Found {num_answers} total answers:')
+  print(all_answers)
 	
-	print('\n--- Reading/processing images...')
-	def load_and_proccess_image(image_path):
+  print('\n--- Reading/processing images...')
+  def load_and_proccess_image(image_path):
     # Load image, then scale and shift pixel values to [-0.5, 0.5]
     im = img_to_array(load_img(image_path))
     return im / 255 - 0.5
 	
-	def read_images(paths):
+  def read_images(paths):
     # paths is a dict mapping image ID to image path
     # Returns a dict mapping image ID to the processed image
     ims = {}
@@ -46,7 +43,7 @@ def setup(use_data_dir):
       ims[image_id] = load_and_proccess_image(image_path)
     return ims
 	
-	if use_data_dir:
+  if use_data_dir:
     # Read images from data/ folder
     def extract_paths(dir):
       paths = {}
@@ -57,36 +54,37 @@ def setup(use_data_dir):
       return paths
 
     train_ims = read_images(extract_paths('/images'))
-	test_ims  = read_images(extract_paths('/images'))
-	im_shape = train_ims[0].shape
-	print(f'Read {len(train_ims)} training images and {len(test_ims)} testing images.')
-	print(f'Each image has shape {im_shape}.')
+  test_ims  = read_images(extract_paths('/images'))
+  im_shape = train_ims[0].shape
+  print(f'Read {len(train_ims)} training images and {len(test_ims)} testing images.')
+  print(f'Each image has shape {im_shape}.')
 	
-	print('\n--- Fitting question tokenizer...')
-	tokenizer = Tokenizer()
-	tokenizer.fit_on_texts(train_qs)
+  print('\n--- Fitting question tokenizer...')
+  tokenizer = Tokenizer()
+  tokenizer.fit_on_texts(train_qs)
 
 	# We add one because the Keras Tokenizer reserves index 0 and never uses it.
-	vocab_size = len(tokenizer.word_index) + 1
-	print(f'Vocab Size: {vocab_size}')
-	print(tokenizer.word_index)
+  vocab_size = len(tokenizer.word_index) + 1
+  print(f'Vocab Size: {vocab_size}')
+  print(tokenizer.word_index)
 	
-	print('\n--- Converting questions to bags of words...')
-	train_X_seqs = tokenizer.texts_to_matrix(train_qs)
-	test_X_seqs = tokenizer.texts_to_matrix(test_qs)
-	print(f'Example question bag of words: {train_X_seqs[0]}')
+  print('\n--- Converting questions to bags of words...')
+  train_X_seqs = tokenizer.texts_to_matrix(train_qs)
+  test_X_seqs = tokenizer.texts_to_matrix(test_qs)
+  print(f'Example question bag of words: {train_X_seqs[0]}')
 
 
-	print('\n--- Creating model input images...')
-	train_X_ims = np.array([train_ims[id] for id in train_image_ids])
-	test_X_ims = np.array([test_ims[id] for id in test_image_ids])
+  print('\n--- Creating model input images...')
+  train_X_ims = np.array([train_ims[id] for id in train_image_ids])
+  test_X_ims = np.array([test_ims[id] for id in test_image_ids])
 
-	print('\n--- Creating model outputs...')
-	train_answer_indices = [all_answers.index(a) for a in train_answers]
-	test_answer_indices = [all_answers.index(a) for a in test_answers]
-	train_Y = to_categorical(train_answer_indices)
-	test_Y = to_categorical(test_answer_indices)	print(f'Example model output: {train_Y[0]}')
+  print('\n--- Creating model outputs...')
+  train_answer_indices = [all_answers.index(a) for a in train_answers]
+  test_answer_indices = [all_answers.index(a) for a in test_answers]
+  train_Y = to_categorical(train_answer_indices)
+  test_Y = to_categorical(test_answer_indices)	
+  print(f'Example model output: {train_Y[0]}')
 
-	return (train_X_ims, train_X_seqs, train_Y, test_X_ims, test_X_seqs,
+  return (train_X_ims, train_X_seqs, train_Y, test_X_ims, test_X_seqs,
           test_Y, im_shape, vocab_size, num_answers,
           all_answers, test_qs, test_answer_indices)  # for the analyze script
